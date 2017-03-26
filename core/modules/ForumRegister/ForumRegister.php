76ac2c733fd2f0f8f037e99859296c2b2e88638a
web541 <web541php@gmail.com>
2017-03-27 07:40:19 +1100

    Initial Commit
    Initial Commit

--- core/modules/ForumRegister/ForumRegister.php
+++ core/modules/ForumRegister/ForumRegister.php
@@ -1,45 +1,45 @@
 <?php
 class ForumRegister extends CodonModule
 {
-		public function __construct()
+    public function __construct()
     {
-			CodonEvent::addListener('ForumRegister');
+	CodonEvent::addListener('ForumRegister');
     }
 
-	 public function EventListener($eventinfo)
-	 {
-			if($eventinfo[0] == 'registration_complete')
-			{
-					$userinfo = $eventinfo[2];
+     public function EventListener($eventinfo)
+     {
+		if($eventinfo[0] == 'registration_complete')
+		{
+			$userinfo = $eventinfo[2];
 
-					$fname = $userinfo['firstname'];
-					$lname = $userinfo['lastname'];
-					$pass = $userinfo['password1'];
-					$email = $userinfo['email'];
-					$ip_address = $userinfo['lastip'];
-					$code = $userinfo['code'];
+			$fname = $userinfo['firstname'];
+			$lname = $userinfo['lastname'];
+			$pass = $userinfo['password1'];
+			$email = $userinfo['email'];
+			$ip_address = $userinfo['lastip'];
+			$code = $userinfo['code'];
 
 
-					$uinfo = PilotData::GetPilotByEmail($userinfo['email']);
+			$uinfo = PilotData::GetPilotByEmail($userinfo['email']);
 
-					$code = PilotData::GetPilotCode($uinfo->code, $uinfo->pilotid);
-					$pilot_id = $fname.' '.$lname;
-					$pilot_id_klein = strtolower($pilot_id);
-					$pilot_id_hyph = $fname.'-'.$lname;
-					$pilot_id_klein_hyph = strtolower($pilot_id_hyph);
+			$code = PilotData::GetPilotCode($uinfo->code, $uinfo->pilotid);
+			$pilot_id = $fname.' '.$lname;
+			$pilot_id_klein = strtolower($pilot_id);
+			$pilot_id_hyph = $fname.'-'.$lname;
+			$pilot_id_klein_hyph = strtolower($pilot_id_hyph);
 
-					$passMD5 = md5($pass);
-					$pass_salt = md5(date('His'));
+			$passMD5 = md5($pass);
+			$pass_salt = md5(date('His'));
 
-					$password = md5($passMD5 . $pass_salt);
+			$password = md5($passMD5 . $pass_salt);
 
-					$tm = time();
-					$member_group_id = '3'; // Change this setting specific to your forum.
+			$tm = time();
+			$member_group_id = '3'; // Change this setting specific to your forum.
 
-					// Insert the user
-					$query = "INSERT INTO core_members (name, member_group_id, email, ip_address, members_seo_name, members_pass_hash, members_pass_salt)
-                    VALUES ('$pilot_id', '$member_group_id', '$email', '{$_SERVER['REMOTE_ADDR']}', '$pilot_id_klein_hyph', '$password', '$pass_salt')";
-      		DB::query($query);
-			}
-	 }
+			// Insert the user
+			$query = "INSERT INTO core_members (name, member_group_id, email, ip_address, members_seo_name, members_pass_hash, members_pass_salt)
+			VALUES ('$pilot_id', '$member_group_id', '$email', '{$_SERVER['REMOTE_ADDR']}', '$pilot_id_klein_hyph', '$password', '$pass_salt')";
+			DB::query($query);
+		}
+      }
 }
